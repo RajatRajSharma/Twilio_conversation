7ec64947eef696aff3da2e011548f9d053e813d8
Neeraj Kumar <neerajk24@users.noreply.github.com>
2024-07-10 12:45:51 +0530

    Add files via upload

---
+++ swaggerConfig.js
@@ -0,0 +1,23 @@
+// swaggerConfig.js
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import express from "express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Documentation",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./api/Routes/*.js"], // Path to the API docs
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const setupSwagger = (app) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+export default setupSwagger;
